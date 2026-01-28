d8a20c50f7404649ed38565a5a59c130fdfe762a
hiba ait abdellah <hibaaitabdellah81@gmail.com>
2026-01-28 14:31:45 +0100

    correct function of refresh url onboading stripe account

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express = require("express")
-import {registerUser,loginUser,getMe, checkAllSellersStatus}  from "../controllers/userController"
+import {registerUser,loginUser,getMe, checkAllSellersStatus, refreshStripeOnboarding}  from "../controllers/userController"
 import { protect } from "../middleware/authMiddleware"
 const router = express.Router()
 router.post("/register",registerUser)
 router.post("/login",loginUser)
 router.get("/me",protect,getMe)
+router.get('/seller/onboarding/refresh', refreshStripeOnboarding); // ✅ Correct - no protect
 
 router.get("/sellerReady",checkAllSellersStatus)
 export default router
