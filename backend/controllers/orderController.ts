117f734dcbf2e542170d0527ae52abf47be4a9ac
hiba ait abdellah <hibaaitabdellah81@gmail.com>
2026-01-29 23:42:46 +0100

    api of get order buyer with detaiils paymemt and addres of buyer in api get order sellers

--- backend/controllers/orderController.ts
+++ backend/controllers/orderController.ts
@@ -2,6 +2,11 @@ import { Request,Response } from 'express';
 import  asyncHandler  from 'express-async-handler';
 import User from '../models/userModel';
 import Order from '../models/ordersModel';
+import dotenv = require("dotenv")
+dotenv.config()
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+
 export const getSellerOrders = asyncHandler(async(req:Request,res:Response) =>{
     try {
         const sellerId = req.user as string
@@ -13,6 +18,7 @@ export const getSellerOrders = asyncHandler(async(req:Request,res:Response) =>{
         const orders = await Order.find({"items.sellerId":sellerId,status:"paid"}).populate({path:"buyer",select:"name"})
         res.status(200).json({
             orders,
+            
             // amount:stripeAccountSeller
 
         })
@@ -32,12 +38,44 @@ export const getAllBuyerOrders = asyncHandler(async(req:Request,res:Response) =>
             return 
         }
         const orders = await Order.find({buyer:buyerId,status:"paid"}).populate({path:"items.sellerId",select:"name email"})
+        const orderPayementDetails = await Promise.all(orders.map(async (or) => {
+            let paymenetDetails = null
+            if(or.stripePayementIntentIdf){
+                try{
+                    console.log("feching payement intent",or.stripePayementIntentIdf)
+                    const paymentIntent = await stripe.paymentIntents.retrieve(or.stripePayementIntentIdf,{expand:["latest_charge"]})
+                    const charge = paymentIntent.latest_charge as Stripe.Charge
+                    console.log("this is charge",charge.id)
+
+                    if(charge){
+                        paymenetDetails = {
+                        transactionId:charge.id,
+                        paymenetIntentId:paymentIntent.id,
+                        amount:paymentIntent.amount / 100,
+                        paymenetStatus:paymentIntent.status,
+                        paymenetDate:new Date(charge.created * 1000),
+                        creditCard: {
+                            last4: charge.payment_method_details?.card?.last4,
+                            brand: charge.payment_method_details?.card?.brand,
+                            expMonth: charge.payment_method_details?.card?.exp_month,
+                            expYear: charge.payment_method_details?.card?.exp_year,
+                            fingerprint: charge.payment_method_details?.card?.fingerprint,
+                        },
+                    }
+                    console.log(paymenetDetails)
+                    }else {
+                        console.log("no chnage found in pyement intent")
+                    }
+                    
+                }catch(err){
+                    console.error("error failting stripe payement ",err)
+                }
+            }
+            return {...or.toObject(),paymenetDetails}
+
+        }))
         res.status(200).json({
-            orders,
-            // creditCart:,
-            // transationId:,
-            // payementDate:,
-            // paymemetStatus:,
+            orders:orderPayementDetails
 
         })
     }catch(err){
