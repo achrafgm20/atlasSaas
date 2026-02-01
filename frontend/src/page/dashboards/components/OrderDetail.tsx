0c03d42fba09a7dbc4f65612de8e705afd4b6861
achraf ait haddouch <achraf.gm12@gmail.com>
2026-02-01 15:17:15 +0100

    merge to get the detailorder

--- frontend/src/page/dashboards/components/OrderDetail.tsx
+++ frontend/src/page/dashboards/components/OrderDetail.tsx
@@ -14,25 +14,38 @@ import { useEffect, useState } from "react"
 
 
 export function OrderDetail({id}) {
-
+    const token = localStorage.getItem("token");
     const [order,setOrder] = useState([])
     const [loading,setLoading] = useState(true)
-    useEffect(()=> {
-        async function fetchUsers(){
-            try{
-              const res = await fetch(`http://localhost:4000/api/orders/getDetailsOrder/${id}`)
-            const data = await res.json()
-            setOrder(data)  
-            }
-            catch(err){
-                console.log(err)
-            }finally{
-                 setLoading(false)
-            }
-        }
-        fetchUsers()
-    },[])
-    console.log(order)
+     const [error, setError] = useState(null);
+   useEffect(() => {
+    if (!id) return;
+
+    async function fetchOrder() {
+      try {
+        const res = await fetch(
+          `http://localhost:4000/api/orders/getDetailsOrder/${id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (!res.ok) throw new Error("Failed to fetch");
+
+        const data = await res.json();
+        setOrder(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchOrder();
+  }, [id]);
+     console.log(order)
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
