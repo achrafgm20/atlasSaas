36d14264b7dc34b2a9b888871744702d192b153e
achraf ait haddouch <achraf.gm12@gmail.com>
2026-02-01 14:54:26 +0100

    add orderdetail

---
+++ frontend/src/page/dashboards/components/OrderDetail.tsx
@@ -0,0 +1,56 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+import { useEffect, useState } from "react"
+
+
+export function OrderDetail({id}) {
+
+    const [order,setOrder] = useState([])
+    const [loading,setLoading] = useState(true)
+    useEffect(()=> {
+        async function fetchUsers(){
+            try{
+              const res = await fetch(`http://localhost:4000/api/orders/getDetailsOrder/${id}`)
+            const data = await res.json()
+            setOrder(data)  
+            }
+            catch(err){
+                console.log(err)
+            }finally{
+                 setLoading(false)
+            }
+        }
+        fetchUsers()
+    },[])
+    console.log(order)
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Show Dialog</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account from our servers. {id}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
